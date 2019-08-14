c1e02e2fecc752e04107173d8112ff8541122b6f
ailem11 <aile.momoh@gmail.com>
2019-08-14 01:48:21 -0400

    Populated the Response categories - Skills, Values, and Interests

--- clientcode/purro/src/app/components/survey-response/survey-response.component.ts
+++ clientcode/purro/src/app/components/survey-response/survey-response.component.ts
@@ -11,7 +11,9 @@ export class SurveyResponseComponent implements OnInit {
 
   surveyResponseList: Response[] = [];
   surveyResponse: Response = {FIRST_NAME:'', LAST_NAME:'', EMAIL:''};
-  //var responsecursor = 
+  responseIndex: number; responseListLength: number = 0;
+  hasPrev: boolean = false; hasNext: boolean = false;
+  hasResponseinDB: boolean = false;
 
   constructor( private dataService: DataService) {
    }
@@ -20,19 +22,62 @@ export class SurveyResponseComponent implements OnInit {
     this.dataService.getSurveyResponses()
     .subscribe(responses=>{
       this.surveyResponseList = responses;
+      this.initCursor();
       console.log('data from dataService: ' + this.surveyResponseList[0].FIRST_NAME);
     })
   };
-
+/*
   getResponse(){
     this.dataService.getSurveyResponse()
     .subscribe(response=>{
       this.surveyResponse = response;
       console.log('data from dataService: ' + this.surveyResponse.FIRST_NAME);
     })
-  };
+  };*/
+
+  initCursor(){
+    if(this.surveyResponseList && this.surveyResponseList.length){
+      this.responseIndex = 0;
+      this.surveyResponse = this.surveyResponseList[this.responseIndex];   
+      this.responseListLength = this.surveyResponseList.length;
+      this.hasResponseinDB = true;
+      this.setCursor();    
+    }
+  }
+
+  setCursor(){
+    this.responseIndex < (this.responseListLength-1)? this.hasNext = true : this.hasNext = false;    
+    this.responseIndex > 0? this.hasPrev = true : this.hasPrev = false;
+  }
+  
+  goToPrevNextResponse(direction){
+    if(direction == "right"){
+      if(this.hasNext){
+        this.responseIndex = this.responseIndex + 1;
+        this.surveyResponse = this.surveyResponseList[this.responseIndex];
+        this.setCursor();        
+      }  
+    }
+    if(direction == "left"){
+      if(this.hasPrev){
+        this.responseIndex = this.responseIndex - 1;
+        this.surveyResponse = this.surveyResponseList[this.responseIndex]; 
+        this.setCursor();       
+      }  
+    }    
+  }
+
+  makeString(arr) {
+    if (arr.length === 1) return arr[0];
+    const firsts = arr.slice(0, arr.length - 1);
+    const last = arr[arr.length - 1];
+    return firsts.join(', ') + ' and ' + last;
+  }
+  
 
   ngOnInit() {
+    this.getResponses();
+   
   }
 
 }
